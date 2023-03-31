0b2f9d2856d51027546e14274bad2f5254a2a0ca
atok624 <84607674+atok624@users.noreply.github.com>
2023-03-31 14:55:18 +0000

    Modify App.js file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import './App.css';
-import React from 'react';
-import Calculator from './components/Calculator';
-import RandomQuote from './components/quotes';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './routes/homePage';
+import MyCalc from './routes/calculatorPage';
+import Quote from './routes/quotesPage';
+import NotMatch from './routes/notFound';
+import Layout from './components/layout';
 
 function App() {
   return (
-    <div className="app">
-      <Calculator />
-      <RandomQuote />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="calculator" element={<MyCalc />} />
+        <Route path="quote" element={<Quote />} />
+        <Route path="*" element={<NotMatch />} />
+      </Route>
+    </Routes>
   );
 }
 
